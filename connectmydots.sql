d5590699737c5cfdff970a24e2996f2ab4f8bfe1
Brad Buchanan <bradley.c.buchanan@gmail.com>
2017-04-26 21:36:27 -0700

    New account table column 'profile'

--- connectmydots.sql
+++ connectmydots.sql
@@ -54,7 +54,8 @@ SET default_with_oids = false;
 
 CREATE TABLE account (
     id character varying(40) NOT NULL,
-    password text NOT NULL
+    password text NOT NULL,
+    profile jsonb DEFAULT '{}'::json
 );
 
 
